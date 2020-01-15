0a2ad3db60139162d26ca2c80bdd763c66f8d500
Drone <37543774+ishanRox@users.noreply.github.com>
2020-01-15 22:49:05 +0530

    Exception Updated

--- Exceptions/ExceptionExample.java
+++ Exceptions/ExceptionExample.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.Exception;
 
-public class ExceptionExample{
+public class ExceptionExample {
     public static void main(String[] args) {
         //syntax of try catch
         try {
@@ -30,28 +30,54 @@ public class ExceptionExample{
 
         //difference between checked and unchecked exception(Examples only not best practises)
         //unchecked- runtime check
-       try{ int i = 10 / 0;}catch(ArithmeticException e){}
+        try {
+            int i = 10 / 0;
+        } catch (ArithmeticException e) {
+        }
         //checked- compile time check
-        try{
-            FileReader fileReader = new FileReader("abc");}catch(FileNotFoundException e){}
+        try {
+            FileReader fileReader = new FileReader("abc");
+        } catch (FileNotFoundException e) {
+        }
 
 //Custome Exception
-try {
-    canIBuyLiquor(14);
-}catch (BuyLiquorException e){
-    System.out.println(e);
-}
+        try {
+            canIBuyLiquor(14);
+        } catch (BuyLiquorException e) {
+            System.out.println(e);
+        }
+
+//cant catch a Parent Exception  from child Exception
+        try {
+            throw new RuntimeException();
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("IndexOutOfBoundsException");
+        }
+     //rethrow exceptions
+        createFile();
+
     }
 
-    public static void canIBuyLiquor(int age) throws BuyLiquorException{
-if (age>18){
-    System.out.println("You can buy !");
-}else{
-    throw new BuyLiquorException("Go to School not to Bar !");
-}
+    public static void createFile() {
+        try {
+            FileReader fileReader = new FileReader("");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Access is denied to C drive try another drive!");
+        }
+
+    }
+
+    public static void canIBuyLiquor(int age) throws BuyLiquorException {
+        if (age > 18) {
+            System.out.println("You can buy !");
+        } else {
+            throw new BuyLiquorException("Go to School not to Bar !");
+        }
     }
 }
 
- class BuyLiquorException extends Exception{
-    BuyLiquorException(String error){ super(error); }
+class BuyLiquorException extends Exception {
+    BuyLiquorException(String error) {
+        super(error);
+    }
 }
